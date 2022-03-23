b1b9d09a6523b07b8a31d1a9059e8891dab9bc32
Iva <samaya@mail.bg>
2022-03-23 14:17:48 +0100

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Infinite summer ice cream store';
+  subtitle = 'Order your favorite delicacies';
+}

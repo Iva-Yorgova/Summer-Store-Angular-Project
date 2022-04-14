518d73c71dd33e93758b7b9a30a254ad5f8a8085
Iva <samaya@mail.bg>
2022-04-14 14:55:09 +0200

    paginator updates

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -4,11 +4,8 @@ import { AuthService } from 'src/app/blog/auth.service';
 import { DialogService } from 'src/app/shared/dialog.service';
 import { Post } from '../post';
 import { PostService } from '../post.service';
-import { FilterPipe } from '../filter.pipe';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Category } from '../category';
-import { MatPaginator} from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-post-list',
@@ -27,10 +24,6 @@ export class PostListComponent implements OnInit  {
   totalLength: any = 11;
   page: number = 1;
 
-  showPosts: any = [];
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
   constructor(
     private postService: PostService, 
     public auth: AuthService,
@@ -41,8 +34,11 @@ export class PostListComponent implements OnInit  {
 
     this.posts = this.postService.getPosts();
     this.categories = this.postService.getCategories();
+    this.posts.subscribe((result: any) => {
+      this.totalLength = result.length;
+      console.log(result.length);
+    });
   }
-
 
   delete(id: string) {
     this.dialogService.openConfirmDialog('Are you sure you want to delete this post?')

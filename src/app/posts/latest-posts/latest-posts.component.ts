16e843e9531002cca6e632c4dac49f5e364b56f0
Iva-Yorgova <84331691+Iva-Yorgova@users.noreply.github.com>
2022-07-07 18:46:51 +0200

    listing 6 posts per page

--- src/app/posts/latest-posts/latest-posts.component.ts
+++ src/app/posts/latest-posts/latest-posts.component.ts
@@ -6,19 +6,20 @@ import { PostService } from '../post.service';
 @Component({
   selector: 'app-latest-posts',
   templateUrl: './latest-posts.component.html',
-  styleUrls: ['./latest-posts.component.scss']
+  styleUrls: ['./latest-posts.component.scss'],
 })
 export class LatestPostsComponent implements OnInit {
-
   posts!: Array<Post>;
   sub: any;
 
   constructor(
     private postService: PostService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.sub = this.postService.getPosts().subscribe(data => this.posts = data.slice(0, 3));
+    this.sub = this.postService
+      .getPosts()
+      .subscribe((data) => (this.posts = data.slice(0, 5)));
   }
-
 }
